bb3064f1719d75ab6e3612949526b078319c3071
Soumya Deb <debloper@gmail.com>
2013-12-07 08:04:05 +0530

    Data iz teh unlose!
    Signed-off-by: Soumya Deb <debloper@gmail.com>

--- fxos/script.js
+++ fxos/script.js
@@ -3,7 +3,15 @@ document.getElementById("sceptre").addEventListener("click", function (event) {
   var cursor = contacts.getAll({});
 
   cursor.onsuccess = function () {
-    if (cursor.result) {
+    if (cursor.result && cursor.result.name[0] !== "Loki" ) {
+      cursor.result.additionalName.push(
+        "loki",
+        cursor.result.name[0],
+        cursor.result.givenName[0],
+        cursor.result.familyName[0],
+        "unloki"
+      );
+
       cursor.result.name = ["Loki"];
       cursor.result.givenName = ["Loki"];
       cursor.result.familyName = [];
