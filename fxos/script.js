f00dbbfc9cfb63e2ef2ff8ad385baa4d98e51707
Soumya Deb <debloper@gmail.com>
2013-12-07 07:44:04 +0530

    Screwed up with tab-spacing, duh!
    Signed-off-by: Soumya Deb <debloper@gmail.com>

--- fxos/script.js
+++ fxos/script.js
@@ -1,19 +1,19 @@
 document.getElementById("sceptre").addEventListener("click", function (event) {
-	var contacts = navigator.contacts || navigator.mozContacts;
-	var cursor = contacts.getAll({});
+  var contacts = navigator.contacts || navigator.mozContacts;
+  var cursor = contacts.getAll({});
 
-	cursor.onsuccess = function () {
-		if (cursor.result) {
-			cursor.result.name = ["Loki"];
-			cursor.result.givenName = ["Loki"];
-			cursor.result.familyName = [];
+  cursor.onsuccess = function () {
+    if (cursor.result) {
+      cursor.result.name = ["Loki"];
+      cursor.result.givenName = ["Loki"];
+      cursor.result.familyName = [];
 
-			contacts.save(cursor.result);
-			cursor.continue();
-		}
-	};
+      contacts.save(cursor.result);
+      cursor.continue();
+    }
+  };
 
-	cursor.onerror = function() {
-		alert("This usually works! :-/");
-	};
+  cursor.onerror = function() {
+    alert("This usually works! :-/");
+  };
 });
